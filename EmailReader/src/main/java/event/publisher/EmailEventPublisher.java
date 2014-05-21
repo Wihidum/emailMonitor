be63f0d364f16fe35fa934aa9b622ba1b95bdbb1
sacjaya <sachinijayasekara.g@gmail.com>
2014-05-21 16:57:04 +0530

    event publisher

---
+++ EmailReader/src/main/java/event/publisher/EmailEventPublisher.java
@@ -0,0 +1,80 @@
+package event.publisher;
+
+import event.publisher.util.KeyStoreUtil;
+import org.wso2.carbon.databridge.agent.thrift.DataPublisher;
+import org.wso2.carbon.databridge.agent.thrift.exception.AgentException;
+import org.wso2.carbon.databridge.commons.exception.*;
+import org.wso2.carbon.databridge.core.exception.DataBridgeException;
+
+import java.net.MalformedURLException;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sachini
+ * Date: 5/21/14
+ * Time: 2:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EmailEventPublisher {
+
+    /**
+     *
+     * @param metaData
+     * @param correlationData
+     * @param payloadData
+     * @throws DataBridgeException
+     * @throws AgentException
+     * @throws MalformedURLException
+     * @throws AuthenticationException
+     * @throws TransportException
+     * @throws MalformedStreamDefinitionException
+     * @throws StreamDefinitionException
+     * @throws DifferentStreamDefinitionAlreadyDefinedException
+     * @throws InterruptedException
+     */
+    public static void publish(Object[] metaData,Object[]correlationData , Object[]payloadData)
+            throws DataBridgeException, AgentException, MalformedURLException,
+            AuthenticationException, TransportException, MalformedStreamDefinitionException,
+            StreamDefinitionException, DifferentStreamDefinitionAlreadyDefinedException,
+            InterruptedException {
+
+        KeyStoreUtil.setTrustStoreParams();
+
+        //according to the convention the authentication port will be 7611+100= 7711 and its host will be the same
+
+        DataPublisher dataPublisher = new DataPublisher("tcp://localhost:7611", "admin", "admin");
+
+        String streamId;
+
+        //sample stream definition
+        try {
+            streamId = dataPublisher.findStream("email_Statistics", "1.0.0");
+        } catch (NoStreamDefinitionExistException e) {
+            streamId = dataPublisher.defineStream("{" +
+                    "  'name':'email_Statistics'," +
+                    "  'version':'1.0.0'," +
+                    "  'nickName': 'Analytics Statistics Information'," +
+                    "  'description': 'Details of Analytics Statistics'," +
+                    "  'metaData':[" +
+                    "          {'name':'emailID','type':'STRING'}" +
+                    "  ]," +
+                    "  'payloadData':[" +
+                    "          {'name':'content','type':'STRING'}," +
+                    "          {'name':'sender','type':'STRING'}" +
+                    "  ]" +
+                    "}");
+
+        }
+        Thread.sleep(1000);
+
+      //  dataPublisher.publish(streamId, new Object[]{"192.168.1.1"}, null, new Object[]{"abc@org1.com", "CEP"});
+
+        dataPublisher.publish(streamId, metaData, correlationData, payloadData);
+
+
+        Thread.sleep(3000);
+        dataPublisher.stop();
+    }
+}
+
---
+++ EmailReader/src/main/java/event/publisher/util/KeyStoreUtil.java
@@ -0,0 +1,48 @@
+package event.publisher.util;/*
+*  Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
+import java.io.File;
+
+public class KeyStoreUtil {
+
+
+
+
+
+
+    //props.load(new FileInputStream(new File(path+"/src/main/resources/properties/settings.properties")));/
+    public static void setTrustStoreParams() {
+        String trustStore = getFilePath().getAbsolutePath();
+        System.setProperty("javax.net.ssl.trustStore", trustStore + "/client-truststore.jks");
+        System.setProperty("javax.net.ssl.trustStorePassword", "wso2carbon");
+
+    }
+
+    public static void setKeyStoreParams() {
+        String keyStore = getFilePath().getAbsolutePath();
+        System.setProperty("Security.KeyStore.Location", keyStore + "/wso2carbon.jks");
+        System.setProperty("Security.KeyStore.Password", "wso2carbon");
+
+    }
+
+
+    private static File  getFilePath(){
+        String path = new File("").getAbsolutePath();
+        return new File(path+"/src/main/resources/security");
+    }
+}

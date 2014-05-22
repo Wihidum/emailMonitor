cd41ce8e58f4557940a29c8d5c397dc41042236d
isururanawaka <irjanith@gmail.com>
2014-05-22 15:13:58 +0530

    mail event model added

--- EmailReader/src/main/java/email/reader/data/MailEvent.java
+++ EmailReader/src/main/java/email/reader/data/MailEvent.java
@@ -1,30 +1,62 @@
 package email.reader.data;
 
 
+import org.compass.core.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MailEvent {
 
-  private String mailfrom;
-  private String mailto;
-  private String subject;
-  private String sendDate;
-  private String[] recipeintList;
+    private List<String> mailfrom;
+    private List<String> recipents;
+    private List<String> mailto;
+    private List<String> mailcc;
+    private List<String> mailbcc;
+    private String subject;
+    private String sendDate;
+
+    private List<String> reply_to;
+    private boolean haveAttachement;
+    private String attachedFileName;
+    private String attachedFileType;
+    private String content;
 
 
-    public String getMailfrom() {
+    public MailEvent() {
+        mailfrom = new ArrayList<String>();
+        recipents = new ArrayList<String>();
+        reply_to = new ArrayList<String>();
+        mailto = new ArrayList<String>();
+        mailcc = new ArrayList<String>();
+        mailbcc = new ArrayList<String>();
+
+    }
+
+    public List<String> getMailfrom() {
         return mailfrom;
     }
 
-    public void setMailfrom(String mailfrom) {
-        this.mailfrom = mailfrom;
+    public void addMailfrom(String mailfrom) {
+        this.mailfrom.add(mailfrom);
     }
 
-    public String getMailto() {
-        return mailto;
+    public List<String> getRecipents() {
+        return recipents;
     }
 
-    public void setMailto(String mailto) {
-        this.mailto = mailto;
+    public void setRecipents(String recipent) {
+        this.recipents.add(recipent);
     }
+
+    public boolean isHaveAttachement() {
+        return haveAttachement;
+    }
+
+    public void setHaveAttachement(boolean haveAttachement) {
+        this.haveAttachement = haveAttachement;
+    }
+
 
     public String getSubject() {
         return subject;
@@ -42,11 +74,102 @@ public class MailEvent {
         this.sendDate = sendDate;
     }
 
-    public String[] getRecipeintList() {
-        return recipeintList;
+
+    public String getAttachedFileType() {
+        return attachedFileType;
     }
 
-    public void setRecipeintList(String[] recipeintList) {
-        this.recipeintList = recipeintList;
+    public void setAttachedFileType(String attachedFileType) {
+        this.attachedFileType = attachedFileType;
     }
+
+    public String getAttachedFileName() {
+        return attachedFileName;
+    }
+
+    public void setAttachedFileName(String attachedFileName) {
+        this.attachedFileName = attachedFileName;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+    public List<String> getMailto() {
+        return mailto;
+    }
+
+    public void addMailto(String mailto) {
+        this.mailto.add(mailto);
+    }
+
+    public List<String> getMailcc() {
+        return mailcc;
+    }
+
+    public void addMailcc(String mailcc) {
+        this.mailcc.add(mailcc);
+    }
+
+    public List<String> getMailbcc() {
+        return mailbcc;
+    }
+
+    public void addMailbcc(String mailbcc) {
+        this.mailbcc.add(mailbcc);
+    }
+
+    public List<String> getReply_to() {
+        return reply_to;
+    }
+
+    public void addReply_to(String reply_to) {
+        this.reply_to.add(reply_to);
+    }
+
+    public void addRecipents(String recipents) {
+        this.recipents.add(recipents);
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("Subject: " + subject);
+        String from = StringUtils.collectionToCommaDelimitedString(mailfrom);
+        buffer.append("\n");
+        buffer.append("From: " + from);
+        String bcc = StringUtils.collectionToCommaDelimitedString(mailbcc);
+        buffer.append("\n");
+        buffer.append("Bcc: " + bcc);
+        String cc = StringUtils.collectionToCommaDelimitedString(mailcc);
+        buffer.append("\n");
+        buffer.append("CC :" + cc);
+        String to = StringUtils.collectionToCommaDelimitedString(mailto);
+        buffer.append("\n");
+        buffer.append("TO :" + to);
+        String allrecp = StringUtils.collectionToCommaDelimitedString(recipents);
+        buffer.append("\n");
+        buffer.append("Recipients:" + allrecp);
+        buffer.append("\n");
+        buffer.append("content:" + content);
+        buffer.append("\n");
+        buffer.append("Attachement:" + isHaveAttachement());
+        buffer.append("\n");
+        if (isHaveAttachement()) {
+            buffer.append("File Name:" + attachedFileName);
+            buffer.append("\n");
+            buffer.append("File type:" + attachedFileType);
+            buffer.append("\n");
+        }
+        buffer.append("send Date:" + sendDate);
+        buffer.append("\n");
+        return buffer.toString();
+    }
+
+
 }
--- EmailReader/src/main/java/email/reader/util/EmailReaderConstants.java
+++ EmailReader/src/main/java/email/reader/util/EmailReaderConstants.java
@@ -4,6 +4,7 @@ package email.reader.util;
 final public  class  EmailReaderConstants {
 
     public static final String IMAPS="imaps";
+    public static final String GIMAPS="gimaps";
     public static final String SMTP_GMAIL_COM="smtp.gmail.com";
     public static final String SETTINGS_FILE_PATH="/src/main/resources/properties/settings.properties";
     public static final String INBOX="inbox";
